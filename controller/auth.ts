1fe2153b63c8dfb60a89f45198837d78dd42cea7
Lucas Nobre <lucasnobre37@outlook.com>
2023-05-12 19:00:08 -0300

    add server controller

---
+++ controller/auth.ts
@@ -0,0 +1,87 @@
+import { Request, Response, NextFunction } from "express";
+
+exports.requestToken = (req: Request, res: Response, next: NextFunction) => {
+  res
+    .status(300)
+    .redirect(
+      "https://marketplace.gohighlevel.com/oauth/chooselocation?response_type=code&redirect_uri=http://localhost:8080/get-token&client_id=645432ff227a27e6f0449de9-lha0u2nb&scope=users.readonly"
+    );
+};
+
+exports.getToken = async (req: Request, res: Response, next: NextFunction) => {
+  const code = req.query.code;
+
+  const url = "https://services.leadconnectorhq.com/oauth/token";
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Accept: "application/json",
+    },
+    body: new URLSearchParams({
+      client_id: "645432ff227a27e6f0449de9-lha0u2nb",
+      client_secret: "2ba9bbf7-6385-41af-a497-7b50ae79e112",
+      grant_type: "authorization_code",
+      code: `${code}`,
+    }),
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    res
+      .status(300)
+      .redirect(
+        `http://localhost:5173/auth?accessToken=${data.access_token}&locationId=${data.locationId}&refreshToken=${data.refresh_token}`
+      );
+  } catch (error) {
+     res
+       .status(300)
+       .redirect(
+         `http://localhost:5173/auth?error=${error}`
+       );
+  }
+};
+
+exports.refreshToken = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const refreshToken = req.query.refreshToken;
+  const url = "https://services.leadconnectorhq.com/oauth/token";
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Accept: "application/json",
+    },
+    body: new URLSearchParams({
+      client_id: "645432ff227a27e6f0449de9-lha0u2nb",
+      client_secret: "2ba9bbf7-6385-41af-a497-7b50ae79e112",
+      grant_type: "refresh_token",
+      refresh_token: `${refreshToken}`,
+    }),
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    if (data.error) {
+      res.status(400).json({ success: false, error: data.error_description });
+    } else {
+      res.status(200).json({
+        success: true,
+        accessToken: data.access_token,
+        locationId: data.locationId,
+        refreshToken: data.refresh_token,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error,
+    });
+  }
+};
